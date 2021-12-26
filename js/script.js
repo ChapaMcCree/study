be488d9549a754833ca2e640b4f7957db4c4b7bc
Chapa <example@gmail.com>
2021-12-26 06:38:49 +0300

    Practice
    Moviedb

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 "use strict";
 
-let incr = 10,
-    decr = 10;
 
-//incr++;
-//decr--;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(incr++);
-console.log(decr--);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последнийх просмотреннных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последнийх просмотреннных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;      
+personalMovieDB.movies[c] = d; 
+
+console.log(personalMovieDB);
